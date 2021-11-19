b076571e4c6173df61f14c69044b1e3616a8f31a
Huy Nguyen <huyscopio007@gmail.com>
2021-11-19 14:35:54 +0700

    roll back api manage user

--- routes/api.php
+++ routes/api.php
@@ -69,7 +69,13 @@ Route::prefix('v1')->group(function () {
         Route::get('/', [RentalController::class, 'index']); // danh sách mượn dành cho user
     });
 
-    Route::middleware(['auth'])->group(function () {        
+    Route::middleware(['auth'])->group(function () {
+        Route::prefix('manage')->group(function (){
+            Route::get('get-user', [ManageUserController::class, 'getUser']); // danh sách user
+            Route::get('get-borrowing', [ManageUserController::class, 'getBorrowing']); // danh sách mượn
+            Route::get('statistics-borrowing-book', [ManageUserController::class, 'statisticsBorrowingBook']);
+        });
+        
         Route::prefix('rental-book')->group(function () {
             Route::post('/', [RentalController::class, 'store']);
             Route::delete('/{_id}', [RentalController::class, 'destroy']);
